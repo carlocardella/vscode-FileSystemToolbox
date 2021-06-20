403c76aebf8d77736d5cc328b1346c510c4ea9e1
Carlo Cardella <Carloc@microsoft.com>
2021-06-19 19:03:25 -0700

    working bug needs improvements

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as folders from "./modules/folders";
 import * as crud from "./modules/crud";
 import * as pathStrings from "./modules/pathStrings";
 import * as pathCompleter from "./modules/pathCompleter";
-import { TextDocument, CompletionList, CompletionItem } from "vscode";
+import { TextDocument, CompletionList, CompletionItem, CompletionItemKind } from 'vscode';
 import * as fs from "fs";
 import * as path from "path";
 
@@ -49,44 +49,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 	// path intellisense
-	   var selector : vscode.DocumentSelector = [{
-        pattern: '*'
-	}];
-	// context.subscriptions.push(vscode.languages.registerCompletionItemProvider(selector, new PathAutocomplete(), "/", "\\"));
-	// context.subscriptions.push(vscode.languages.registerCompletionItemProvider(selector, { provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) { pathCompleter.provideCompletionItems();}}, "/"));
-
-	// const provider2 = vscode.languages.registerCompletionItemProvider(
-    //     "*",
-    //     // {
-    //     //     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-    //     //         // get all text until the `position` and check if it reads `console.`
-    //     //         // and if so then complete if `log`, `warn`, and `error`
-    //     //         const linePrefix = document.lineAt(position).text.substr(0, position.character);
-    //     //         if (!linePrefix.endsWith("console.")) {
-    //     //             return undefined;
-    //     //         }
-
-    //     //         return [
-    //     //             new vscode.CompletionItem("log", vscode.CompletionItemKind.Method),
-    //     //             new vscode.CompletionItem("warn", vscode.CompletionItemKind.Method),
-    //     //             new vscode.CompletionItem("error", vscode.CompletionItemKind.Method),
-    //     //         ];
-    //     //     },
-    //     // },
-	// 	pathCompleter.provideCompletionItems(),
-	// 	// new pathCompleter.pathCompletion(),
-    //     "/" // triggered whenever a '.' is being typed
-    // );
-
 	const provider2 = vscode.languages.registerCompletionItemProvider(
 		"*",
         {
 			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-				
-				let folderItems = fs.readdirSync("C:/Temp/", null);
+				let currentFolder = pathCompleter.getUserPath();
+				if (!currentFolder) { return; }
+				let folderItems = fs.readdirSync(currentFolder, null);
 
 				let completionItems = folderItems.map((item) => {
-					return new vscode.CompletionItem(item);
+					return new vscode.CompletionItem(item, CompletionItemKind.Value);
 				});
 
 				return completionItems;
--- src/modules/pathCompleter.ts
+++ src/modules/pathCompleter.ts
@@ -1,8 +1,85 @@
+import {
+    Range,
+    workspace,
+    Uri,
+    FileType,
+    CompletionItem,
+    CompletionItemKind,
+    CompletionItemProvider,
+    TextDocument,
+    Position,
+    CompletionList,
+    TreeItem,
+    CancellationToken,
+    CompletionContext,
+    ProviderResult,
+} from "vscode";
+import { getActiveEditor, log } from "./shared";
+import * as path from "path";
+import * as fs from "fs";
 
-export function provideCompletionItems() {
+/*
+// @TODO: configuration
+// separator format: system, win32, posix
+// trigger character: /, \, drive letter
+// recognize and trigger home directory notation: ~\, HOME\, $HOME\, $env: if powershell
+*/
 
+export async function test() {
+    // let items = await workspace.fs.readDirectory(Uri.parse("c:/Temp/"));
+    // console.log(items);
+
+    const editor = getActiveEditor();
+    let s = editor?.document.getWordRangeAtPosition(editor.selection.active, RegExp('[^"]+$'));
+    console.log(getTextFromRange(s!));
 }
 
-export function test() {
-    
+// /**
+//  * Returns the content of the passed in folder
+//  * @export
+//  * @param {Uri} folderUri Uri of the folder to return the content from
+//  * @return {*}  {Promise<[string, FileType][]>}
+//  */
+// async function getFolderContent(folderUri: Uri): Promise<[string, FileType][]> {
+//     return Promise.resolve(workspace.fs.readDirectory(folderUri));
+// }
+
+// /**
+//  * Returns the path the user entered in the text editor
+//  * @export
+//  * @return {*}  {(Promise<string | undefined>)}
+//  */
+// export async function getUserPath(): Promise<string | undefined> {
+//     const editor = getActiveEditor();
+
+//     let range = editor?.document?.getWordRangeAtPosition(getActiveEditor()?.selection.active!, undefined);
+//     if (!range) {
+//         return Promise.reject();
+//     }
+
+//     return Promise.resolve(await getTextFromRange(range));
+// }
+
+// /**
+//  * Returns text from the selected Range
+//  * @param {Range} range The Range to return text from
+//  * @return {*}  {(Promise<string | undefined>)}
+//  */
+// export async function getTextFromRange(range: Range): Promise<string | undefined> {
+//     return Promise.resolve(getActiveEditor()?.document.getText(range));
+// }
+
+export function getUserPath(): string {
+    const editor = getActiveEditor();
+
+    let range = editor?.document.getWordRangeAtPosition(editor.selection.active, RegExp('[^"]+$'));
+    if (!range) {
+        return "";
+    }
+
+    return getTextFromRange(range);
+}
+
+export function getTextFromRange(range: Range): string {
+    return getActiveEditor()?.document.getText(range)!;
 }
